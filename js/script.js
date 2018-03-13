dd7e94960ee908aafcccebc7291eebbc6d009541
Marcin Trawiński <trawinski1@wp.pl>
2018-03-13 18:39:52 +0100

    Add slider active caption/thumbnail change while changeing photo

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const headerSlider = {
   images: document.querySelectorAll('.header__img'),
   controls: document.querySelectorAll('.header__arrow-box'),
+  caption: document.querySelectorAll('.header__list li'),
+  activeCLassName: 'header__listItem--active',
   index: 0
 };
 
@@ -9,19 +11,23 @@ const headerSlider = {
 const projectsSlider = {
   images: document.querySelectorAll('.latest__img'),
   controls: document.querySelectorAll('.latest__arrow-box'),
+  caption: document.querySelectorAll('.latest__thumbnail'),
+  activeCLassName: 'latest__thumbnail--active',
   index: 3
 };
 
 function slider(sliderObj) {
 
   function reset() {
-   sliderObj.images.forEach(image => {
+   sliderObj.images.forEach((image, imgIndex) => {
       image.style.display = 'none';
+      sliderObj.caption[imgIndex].classList.remove(sliderObj.activeCLassName);
    });
   }
 
   function startSlide() {
     sliderObj.images[sliderObj.index].style.display = 'block';
+    sliderObj.caption[sliderObj.index].classList.add(sliderObj.activeCLassName);
   }
 
   sliderObj.controls.forEach(arrow => {
@@ -42,6 +48,8 @@ function slider(sliderObj) {
       }
 
       sliderObj.images[sliderObj.index].style.display = 'block';
+      sliderObj.caption[sliderObj.index].classList.add(sliderObj.activeCLassName);
+      
 
     } else {
 
@@ -52,6 +60,8 @@ function slider(sliderObj) {
       }
 
       sliderObj.images[sliderObj.index].style.display = 'block';
+      sliderObj.caption[sliderObj.index].classList.add(sliderObj.activeCLassName);
+      
     }
   }
 
