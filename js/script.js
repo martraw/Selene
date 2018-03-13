0ef223b6c458945f500a9795d4459504890bd8ec
Marcin Trawiński <trawinski1@wp.pl>
2018-03-13 20:13:26 +0100

    Refactor slider code

--- js/script.js
+++ js/script.js
@@ -1,73 +1,84 @@
  /* Header slider object */
-const headerSlider = {
-  images: document.querySelectorAll('.header__img'),
-  controls: document.querySelectorAll('.header__arrow-box'),
-  caption: document.querySelectorAll('.header__list li'),
-  activeCLassName: 'header__listItem--active',
-  index: 0
-};
+ const headerSlider = {
+   images: document.querySelectorAll('.header__img'),
+   controls: document.querySelectorAll('.header__arrow-box'),
+   caption: document.querySelectorAll('.header__list li'),
+   activeCLassName: 'header__listItem--active',
+   pictureNum: document.querySelector('.header__picture-number'),
+   index: 0
+ };
 
  /* Projects slider object */
-const projectsSlider = {
-  images: document.querySelectorAll('.latest__img'),
-  controls: document.querySelectorAll('.latest__arrow-box'),
-  caption: document.querySelectorAll('.latest__thumbnail'),
-  activeCLassName: 'latest__thumbnail--active',
-  index: 3
-};
+ const projectsSlider = {
+   images: document.querySelectorAll('.latest__img'),
+   controls: document.querySelectorAll('.latest__arrow-box'),
+   caption: document.querySelectorAll('.latest__thumbnail'),
+   activeCLassName: 'latest__thumbnail--active',
+   index: 3
+ };
 
-function slider(sliderObj) {
+ const imageSlider = {
+   /* Remove display block from all images also remove "active" class from caption */
+   reset: function (obj) {
+     obj.images.forEach((image, imgIndex) => {
+       image.style.display = 'none';
+       obj.caption[imgIndex].classList.remove(obj.activeCLassName);
+     });
+   },
+   /* Set slider to initial state on page load */
+   initiate: function (obj) {
+     obj.images[obj.index].style.display = 'block';
+     obj.caption[obj.index].classList.add(obj.activeCLassName);
+   },
+   /* Add click event to slider controls */
+   addClick: function (obj) {
+     obj.controls.forEach(arrow => {
+       arrow.addEventListener('click', e => {
+         this.slide(e, obj)
+       })
+     })
+   },
+   /* Handles click event and image change */
+   slide: function (e, obj) {
+     this.reset(obj);
+     if (e.target.classList.contains('arrow--right')) {
+       obj.index++;
 
-  function reset() {
-   sliderObj.images.forEach((image, imgIndex) => {
-      image.style.display = 'none';
-      sliderObj.caption[imgIndex].classList.remove(sliderObj.activeCLassName);
-   });
-  }
+       if (obj.index > obj.images.length - 1) {
+         obj.index = 0;
+       }
 
-  function startSlide() {
-    sliderObj.images[sliderObj.index].style.display = 'block';
-    sliderObj.caption[sliderObj.index].classList.add(sliderObj.activeCLassName);
-  }
+       obj.images[obj.index].style.display = 'block';
+       obj.caption[obj.index].classList.add(obj.activeCLassName);
+       this.pictureNum(obj);
 
-  sliderObj.controls.forEach(arrow => {
-    arrow.addEventListener('click', (e) => {
-      slide(e);
-    });
-  });
+     } else {
+       obj.index--;
 
-  function slide(event) {
-    reset();
+       if (obj.index < 0) {
+         obj.index = obj.images.length - 1;
+       }
 
-    if (event.target.classList.contains('arrow--right')) {
-      
-      sliderObj.index ++;
+       obj.images[obj.index].style.display = 'block';
+       obj.caption[obj.index].classList.add(obj.activeCLassName);
+       this.pictureNum(obj);
+     }
+   },
+   /* Handle large picture number in header slider */
+   pictureNum: function (obj) {
+     if (obj.pictureNum) {
+       obj.pictureNum.innerHTML = `0${obj.index + 1}`;
+     } else {
+       return;
+     }
+   },
 
-      if (sliderObj.index > sliderObj.images.length -1) {
-        sliderObj.index = 0;
-      }
+   startSlide: function (obj) {
+     this.reset(obj);
+     this.initiate(obj);
+     this.addClick(obj);
+   },
+ }
 
-      sliderObj.images[sliderObj.index].style.display = 'block';
-      sliderObj.caption[sliderObj.index].classList.add(sliderObj.activeCLassName);
-      
-
-    } else {
-
-      sliderObj.index --;
-
-      if (sliderObj.index < 0) {
-        sliderObj.index = sliderObj.images.length -1;
-      }
-
-      sliderObj.images[sliderObj.index].style.display = 'block';
-      sliderObj.caption[sliderObj.index].classList.add(sliderObj.activeCLassName);
-      
-    }
-  }
-
-  reset();
-  startSlide();
-}
-
-slider(headerSlider);
-slider(projectsSlider);
+ imageSlider.startSlide(headerSlider);
+ imageSlider.startSlide(projectsSlider);
